9ae348e5fc88d8380981ce3ad3290c7485cccfa4
Cecilia Torreira <celia.umbreon@gmail.com>
2023-06-02 12:21:05 +0100

    Rutas de crear comentario y mostrar comentarios sobre una receta específica terminadas y funcionan

--- routes/recipes.routes.js
+++ routes/recipes.routes.js
@@ -1,6 +1,11 @@
 const router = require("express").Router();
+
+//* Modelos importados
 const Recipe = require("../models/Recipe.model");
 const User = require("../models/User.model"); // Modelo de User importado para cuando quiera asociar una receta a un usuario
+const Comment = require("../models/Comment.model")
+
+//* Middlewares
 const isAuthenticated = require("../middlewares/isAuthenticated");
 
 // GET "/api/recipes" => Envían al front end todas las recetas, mostrando nombre (añadiré más según como se vean en el FE)
@@ -123,5 +128,56 @@ router.post("/:recipeId/favourite", isAuthenticated, async (req, res, next) => {
     next(error);
   }
 });
+
+
+// COMENTARIOS EN RECETAS
+
+
+// GET "/api/recipes/:recipeId/comments" => Se envían al FE los comentarios que haya en una receta
+
+router.get("/:recipeId/comments", async (req, res, next)=>{
+ const { recipeId } = req.params
+
+ try {
+    const recipeComments = await Comment.find({recipe: recipeId})  // Buscamos comentarios hechos únicamente sobre la receta con dicho ID específico (recipeId) 
+    console.log(recipeComments); // [ ] No tenemos ni un único comentario todavía, creo que si al menos me devuelve un array vacío es porue está bien
+    res.json(recipeComments)    //! Actualizado, ahora sale en la búsqueda el comentario que creé con la siguiente ruta
+ } catch (error) {
+    next(error)
+ }
+
+})
+
+
+// POST "/api/recipes/:recipeId/comments" => Se reciben los datos del FE para crear un nuevo comentario y añadirlo a la base de datos
+
+ router.post("/:recipeId/comments", isAuthenticated, async(req, res, next)=>{
+const {recipeId} = req.params
+const {description, rating} = req.body
+  try {
+    if (!description || rating === 0) {
+        res.status(400).json({
+            errorMessage: "Some mandatory fields are empty, please try again.",
+          })
+
+          return;
+    }
+    await Comment.create({
+    description,
+    rating,
+    creator: req.payload._id, // Funciona, se agrega el comentario a la receta con creator: Id del usuario activo
+    recipe: recipeId          // Funciona igualmente y se crea un comentario con recipe: id de la receta pertinente sobre la que comentamos
+    })
+    res.json("Comment posted")
+
+
+  } catch (error) {
+    next(error)
+  }
+
+
+})
+ 
+
 
 module.exports = router;
